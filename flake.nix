224563c848b4b64760e2d95bad485cdcd58b19e6
Adrien Bustany <adrien@bustany.org>
2023-05-09 14:47:51 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+
+{
+  description = "Dev shell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-22.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            (rust-bin.stable."1.69.0".default.override {
+              extensions = ["rust-analyzer" "rust-src"];
+            })
+            gtk3
+          ];
+        };
+      }
+    );
+}
